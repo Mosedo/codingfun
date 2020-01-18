eb412ebe49d8c42fa94c4d47e612f09e0036ae87
Mosedo <39417893+Mosedo@users.noreply.github.com>
2020-01-18 21:28:57 +0300

    Update recursive_binary_search.py

--- recursive_binary_search.py
+++ recursive_binary_search.py
@@ -6,12 +6,16 @@ def binary_search_recursive(array,key,left,right):
 	if left > right:
 		return False
 	else:
+		#defining the middle of the list
 		mid=int((left+right)/2)
+		#if the middle value is our target,return True, our recursion base case
 		if key==array[mid]:
 			return True
 		elif key < array[mid]:
+			#else we call the function recursively towards the left if the key is less than the middle
 			return binary_search_recursive(array,key,left,mid-1)
 		elif key > array[mid]:
+			#call the function recursively to the right if the key is greater than the middle
 			return binary_search_recursive(array,key,mid+1,right)
 
 
